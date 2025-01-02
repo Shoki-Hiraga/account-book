b313d47473391507eb8c86fbc775bab86cbdba00
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-01-03 03:17:58 +0900

    サービスプロバイダーの修正（マイグレーションエラーになってしまう）

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\pagination\Paginator;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use App\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,7 +25,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         Paginator::useBootstrapFour();
-        View::share('categories', $this->shareCategories());
+        if (Schema::hasTable('categories')) {
+            View::share('categories', $this->shareCategories());
+        } else {
+            View::share('categories', collect()); // 空のコレクションを共有
+        }
     }
     private function shareCategories(){
         return Category::all();
